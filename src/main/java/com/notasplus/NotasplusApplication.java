886260ce8d2a43b17aacd1ba70b24853fb31a2f2
miguelmartin90 <agnostostheos90@gmail.com>
2024-08-28 01:41:47 -0500

    Proyecto Spring con Maven y dependencias inicializado

---
+++ src/main/java/com/notasplus/NotasplusApplication.java
@@ -0,0 +1,15 @@
+package com.notasplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotasplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotasplusApplication.class, args);
+		System.out.println("\n--> NOTASPLUS en ejecución");
+		System.out.println("");
+	}
+
+}
