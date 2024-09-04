47c7deed1ce1b0177d795293fa842236d214e951
DeiNas77 <deinas.44Remastered@gmail.com>
2024-09-04 17:16:09 -0500

    Add login and footer section and add asset

---
+++ NotaPlus/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className="flex justify-center">
+      <a href="#" className="text-[#ffffff] mt-[2rem]">
+        Politicas de privacidad
+      </a>
+    </footer>
+  );
+}
+
+export default Footer;
