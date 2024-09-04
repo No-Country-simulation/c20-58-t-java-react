800894167b7bd1eb2b5dd58db8d2937943866503
DeiNas77 <deinas.44Remastered@gmail.com>
2024-09-04 17:13:23 -0500

    Add New Bg in Index.Html and Comment Header and Navbar

--- NotaPlus/src/components/NavBar.jsx
+++ NotaPlus/src/components/NavBar.jsx
@@ -1,16 +1,11 @@
-
-export const NavBar = () => {
-  return (
-    <nav className="h-[55px] px-5 sticky top-0 z-50 py-[.8rem] bg-[#e91e63] lg:px-20 flex justify-between text-white">
-    <ul className="flex items-center space-x-2 lg:space-x-10">
-      <li>Inicio</li>
-      <li className="">Mensaje</li>
-      <li>Perfil</li>
-    </ul>
-</nav>
-  )
-}
-
-
-
-
+// export const NavBar = () => {
+//   return (
+//     <nav className="h-[55px] px-5 sticky top-0 z-50 py-[.8rem] bg-[#e91e63] lg:px-20 flex justify-between text-white">
+//       <ul className="flex items-center space-x-2 lg:space-x-10">
+//         <li>Inicio</li>
+//         <li className="">Mensaje</li>
+//         <li>Perfil</li>
+//       </ul>
+//     </nav>
+//   );
+// };
