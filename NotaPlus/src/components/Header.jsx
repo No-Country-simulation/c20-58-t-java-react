2235afdfeaf0f46e0b9e74a932ee749d12260818
Luma <114626233+Luma2001@users.noreply.github.com>
2024-09-04 19:56:19 -0300

    Update Header.jsx
    se descomenta header

--- NotaPlus/src/components/Header.jsx
+++ NotaPlus/src/components/Header.jsx
@@ -1,11 +1,11 @@
-// import logo from "../asset/logoNotaPlus.png";
-// import { NavBar } from "./NavBar";
+import logo from "../asset/logoNotaPlus.png";
+import { NavBar } from "./NavBar";
 
-// export const Header = () => {
-//  return (
-//     <header className="flex place-content-between items-center mb-8">
-//       <img src={logo} alt="Logo de NotaPlus" className="h-[55px]" />
-//       {/* <NavBar /> */}
-//     </header>
-//   );
-// }; 
+export const Header = () => {
+  return (
+     <header className="flex place-content-between items-center mb-8">
+       <img src={logo} alt="Logo de NotaPlus" className="h-[55px]" />
+       <NavBar /> 
+     </header>
+  );
+}; 
