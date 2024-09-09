0a7bcdf4aa6d93c1337f202f8514d132cb173d00
DeiNas77 <deinas.44Remastered@gmail.com>
2024-09-09 15:00:51 -0500

    [modified]: add new style in index.css and add store in main

--- NotaPlus/src/App.jsx
+++ NotaPlus/src/App.jsx
@@ -1,16 +1,10 @@
 // import "./App.css";
 
 import AppRouter from "./routers/AppRouter";
-
-
 function App() {
   return <>
-
     <main className="bg-custom p-4 h-screen">
-      
       <AppRouter/>
-
-
     </main>
   </>;
 }
--- NotaPlus/src/page/Home.jsx
+++ NotaPlus/src/page/Home.jsx
@@ -1,23 +1,27 @@
-import logo from "../asset/notaPlusLogo.svg"
-import Footer from  "../components/Footer"
+import logo from "../asset/notaPlusLogo.svg";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
-
-  <section className="flex flex-col justify-center min-h-[90vh]">
-    <div className="flex flex-col items-center gap-[35px]">
-        <h2 className="font-bold text-white text-[30px] uppercase">Bienvenidos</h2>
-        <img src={logo} alt="imagen de bienvenida" className="w-[213.59px] h-[144px]"/>
-        <button className="bg-palatinateBlue_Button w-[291px] h-[48px] rounded-lg py-[12px] px-[16px] text-white ease-in-out duration-300 hover:bg-mexicanPink_logo hover:scale-90 "><a href="http://localhost:5173/login"><span>Iniciar Sesión</span></a></button>
+    <section className="flex flex-col justify-center min-h-[90vh]">
+      <div className="flex flex-col items-center gap-[35px]">
+        <h2 className="font-bold text-white text-[30px] uppercase">
+          Bienvenidos
+        </h2>
+        <img
+          src={logo}
+          alt="imagen de bienvenida"
+          className="w-[213.59px] h-[144px]"
+        />
+        <button className="bg-palatinateBlue_Button w-[291px] h-[48px] rounded-lg py-[12px] px-[16px] text-white ease-in-out duration-300 hover:bg-mexicanPink_logo hover:scale-90 ">
+          <a href="http://localhost:5173/login">
+            <span>Iniciar Sesión</span>
+          </a>
+        </button>
       </div>
-    <Footer/>
-  </section>
+      <Footer />
+    </section>
+  );
+};
 
-  )
-}
-
-export default Home
-
-
-
-
+export default Home;
